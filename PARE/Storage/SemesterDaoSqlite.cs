43417c7f8f2ef0438f90953d8cb276829f7b8c98
ClotildeMALO <clotilde.malo@iut-dijon.u-bourgogne.fr>
2024-10-17 20:41:40 +0200

    feat : ListAll Semester fonctionnel

--- PARE/Storage/SemesterDaoSqlite.cs
+++ PARE/Storage/SemesterDaoSqlite.cs
@@ -1,8 +1,10 @@
-﻿using Model;
+﻿using Microsoft.Data.Sqlite;
+using Model;
 using Storage.InterfaceDAO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,7 +27,36 @@ namespace Storage
         }
         public Semester[] ListAll()
         {
-            throw new NotImplementedException();
+            List<Semester> semesters = new List<Semester>();
+            db.Connection.Open();
+            var cmd = db.Connection.CreateCommand();
+            cmd.CommandText = "SELECT" +
+                               " idSemester" +
+                               ", nameSemester" +
+                               ", numberGroupTp" +
+                               " FROM Semester;";
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    semesters.Add(Reader2Semester(reader));
+                }
+            }
+            return semesters.ToArray();
+        }
+
+        /// <summary>
+        /// Convertit un reader en semestre
+        /// </summary>
+        /// <param name="reader">reader associé au semestre</param>
+        /// <returns>semestre converti</returns>
+        private Semester Reader2Semester(SqliteDataReader reader)
+        {
+            Semester semester = new Semester();
+            semester.Id = Convert.ToInt32(reader["idSemester"]);
+            semester.Name = reader["nameSemester"].ToString();
+            semester.NbTpGroups = Convert.ToInt32(reader["numberGroupTp"]);
+            return semester;
         }
     }
 }
