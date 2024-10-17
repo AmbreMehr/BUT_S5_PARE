5176fdb5a40e9e3882e622e340b26c3389a65fda
ClotildeMALO <clotilde.malo@iut-dijon.u-bourgogne.fr>
2024-10-17 10:38:27 +0200

    feat : création des models

---
+++ PARE/Model/Module.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Classe qui représente la ressource (=module) 
+    /// <author>Clotilde MALO</author>
+    /// </summary>
+    public class Module
+    {
+        private Semester semester;
+        private User supervisor;
+        private int id;
+        private string name;
+        private int hoursTd;
+        private int hoursTp;
+        private int hoursCM;
+        private int weekBegin;
+        private int weekEnd;
+
+        /// <summary>
+        /// Get et set du semestre dans lequel le module intervient
+        /// </summary>
+        public Semester Semester { get => semester; set => semester = value; }
+
+        /// <summary>
+        /// Get et set de l'utilisateur qui est le superviseur du module
+        /// </summary>
+        public User Supervisor { get => supervisor; set => supervisor = value; }
+
+        /// <summary>
+        /// Get et set de l'id du module
+        /// </summary>
+        public int Id { get => id; set => id = value; }
+
+        /// <summary>
+        /// Get et set du nom du module
+        /// </summary>
+        public string Name { get => name; set => name = value; }
+
+
+        /// <summary>
+        /// Get et set du nbre d'heures de TD
+        /// </summary>
+        public int HoursTd
+        {
+            get => hoursTd;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Les nombre d'heures de TD ne peuvent pas être négatives");
+                }
+                else
+                {
+                    hoursTd = value;
+                }
+
+            }
+        }
+        /// <summary>
+        /// Get et set du nbre d'heures de TP
+        /// </summary>
+        public int HoursTp
+        {
+            get => hoursTp;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Les nombre d'heures de TP ne peuvent pas être négatives");
+                }
+                else
+                {
+                    hoursTp = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get et set du nbre d'heures de CM
+        /// </summary>
+        public int HoursCM
+        {
+            get => hoursCM;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Les nombre d'heures de CM ne peuvent pas être négatives");
+                }
+                else
+                {
+                    hoursCM = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Get et set de la semaine de début
+        /// <exception>La semaine de début ne peut ni être négative ni supérieur à 53</exception>
+        /// </summary>
+        public int WeekBegin {
+            get => weekBegin;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("La semaine de début ne peut pas être négative");
+                }
+                else if (value > 53)
+                {
+                    throw new ArgumentException("La semaine de début ne peut pas être supérieur à 53");
+                }
+                else
+                {
+                    weekBegin = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get et set de la semaine de fin
+        /// <exception>La semaine de fin ne peut ni être négative ni supérieur à 53</exception>
+        /// </summary>
+        public int WeekEnd
+        {
+            get => weekEnd;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("La semaine de fin ne peut pas être négative");
+                }
+                else if (value > 53)
+                {
+                    throw new ArgumentException("La semaine de fin ne peut pas être supérieur à 53");
+                }
+                else
+                {
+                    weekEnd = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Constructeur de module avec tous les paramètres sauf supervisor
+        /// </summary>
+        /// <param name="semester">semestre dans lequel le module intervient</param>
+        /// <param name="id">id module</param>
+        /// <param name="name">nom module</param>
+        /// <param name="hoursTd">nbre heure de TD</param>
+        /// <param name="hoursTp">nbre heure TP</param>
+        /// <param name="hoursCM">nbre heure CM</param>
+        /// <param name="weekBegin">numéro semaine début</param>
+        /// <param name="weekEnd">numéro semaine de fin</param>
+        public Module(Semester semester, User supervisor, int id, string name, int hoursTd, int hoursTp, int hoursCM, int weekBegin, int weekEnd)
+        {
+            this.semester = semester;
+            this.supervisor = supervisor;
+            this.id = id;
+            this.name = name;
+            this.hoursTd = hoursTd;
+            this.hoursTp = hoursTp;
+            this.hoursCM = hoursCM;
+            this.weekBegin = weekBegin;
+            this.weekEnd = weekEnd;
+        }
+
+        /// <summary>
+        /// Constructeur de module avec seulement le semestre concernéé
+        /// </summary>
+        public Module(Semester semester)
+        {
+            this.semester = semester;
+        }
+
+
+    }
+}
