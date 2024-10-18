18bd4b4033535cc4a7607853d6405d1b6b158156
StephaneBasset <104894715+StephaneBasset@users.noreply.github.com>
2024-10-18 11:32:35 +0200

    Feat: Implémentation de TeachersVM

--- PARE/IHM-Model/TeachersVM.cs
+++ PARE/IHM-Model/TeachersVM.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IHM_Model
 {
+    /// <summary>
+    /// La classe `TeachersVM` gère les teacher dans l'application PARE.
+    /// </summary>
+    /// <author>Stéphane BASSET</author>
     public class TeachersVM : BaseVM
     {
+        private Teacher[]_model;
+        private Module _module;
+
+
+        /// <summary>
+        /// Récupère les Teacher pour un modules donné.
+        /// </summary>
+        /// <returns>Tableau des teacher pour les modules</returns>
+        /// <author>Stéphane BASSET</author>
+        public Teacher[] GetTeachersByModule(Module module)
+        {
+            // A faire: Implémenter la logique ici pour retourner les teacher associés à un   modules 
+            return _model; // Retourne le tableau des teacher, peut être modifié selon la logique
+        }
+
     }
 }
