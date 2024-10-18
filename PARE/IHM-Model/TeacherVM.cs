dded2b85f5947f99075ac4d08bf2f9e1a2f574db
StephaneBasset <104894715+StephaneBasset@users.noreply.github.com>
2024-10-18 13:32:25 +0200

    Feat: Implémentation de TeacherVM

--- PARE/IHM-Model/TeacherVM.cs
+++ PARE/IHM-Model/TeacherVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace IHM_Model
 {
     public class TeacherVM : BaseVM
     {
+        private Teacher _model;
+        public Teacher Model
+            { 
+            get { return _model;}
+            set { 
+                _model = value;
+                NotifyChange("Teacher");
+            }
+        }
+
+        public void UpdateTeacher(Teacher teacherModify)
+        {
+            _model = teacherModify;
+        }
+
+        public void DeleteTeacher(Teacher teacherDelete)
+        {
+            _model = teacherDelete;
+        }
+
+        public void CreateTeacher( Teacher teacherToCreate)
+        {   
+            _model = teacherToCreate;
+        }
     }
 }
