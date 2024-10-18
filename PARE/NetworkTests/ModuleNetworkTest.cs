d03e9e602961ac9b28fc0fa5194d977df9384c46
AmbreMehr <ambre.mehr@iut-dijon.u-bourgogne.fr>
2024-10-18 11:11:48 +0200

    feat : Network GetAllModules

---
+++ PARE/NetworkTests/ModuleNetworkTest.cs
@@ -0,0 +1,21 @@
+﻿using Model;
+using Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkTests
+{
+    public class ModuleNetworkTest
+    {
+        [Fact]
+        public async void GetAll()
+        {
+            IModuleNetwork network = new ModuleNetwork();
+            IEnumerable<Module> modules = await network.GetAllModules();
+            Assert.NotEmpty(modules);
+        }
+    }
+}
