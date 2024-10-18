36a4a2635b73bc9e0c3b085334895e665e150ccd
StephaneBasset <104894715+StephaneBasset@users.noreply.github.com>
2024-10-18 14:17:02 +0200

    Feat: implémentation de UsersVM

--- PARE/IHM-Model/UsersVM.cs
+++ PARE/IHM-Model/UsersVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,48 @@ using System.Threading.Tasks;
 
 namespace IHM_Model
 {
+    /// <summary>
+    /// La classe UsersVM gère les utilisateurs
+    /// Le setter déclenche une notification de changement.
+    /// </summary>
+    /// /// <author>Stéphane BASSET</author>
     public class UsersVM : BaseVM
     {
+        private User[]_models;
+
+        /// <summary>
+        /// Propriété pour obtenir ou définir le tableau des utilisateurs.
+        /// </summary>
+        public User[] Users
+            { 
+            get { return _models; } 
+            set { 
+                _models = value;
+                NotifyChange("Users");
+            }
+        
+        }
+
+        /// <summary>
+        /// Récupère tous les utilisateurs.
+        /// </summary>
+        /// <returns>Tableau des modules pour le semestre</returns>
+        /// <author>Stéphane BASSET</author>
+        public User[] GetAllUsers()
+        {
+            return _models;
+        }
+
+        /// <summary>
+        /// Récupère tous lesutilisateur par role.
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        /// <author>Stéphane BASSET</author>
+        public User[] GetAllUsersByRole(Role role)
+        {
+            User[] users = GetAllUsers();
+            return users;
+        }
     }
 }
