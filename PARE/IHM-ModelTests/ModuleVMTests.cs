c93eff1926c8df3c870a079810580e5c6ed566f9
Lucas Prunier <91905057+LucasLaPrune@users.noreply.github.com>
2024-11-27 14:54:29 +0100

    FEAT Création de tests pour l'IHM-Model + tests sur ModuleVM

---
+++ PARE/IHM-ModelTests/ModuleVMTests.cs
@@ -0,0 +1,80 @@
+using IHM_Model;
+using Model;
+
+namespace IHM_ModelTests
+{
+    public class ModuleVMTests
+    {
+        [Fact]
+        public void TestGetSet()
+        {
+            Module module = new Module { Name = "Old Name" };
+            ModuleVM vm = new ModuleVM(module);
+
+            vm.Name = "New Name";
+
+            Assert.Equal("New Name", vm.Name);
+            Assert.Equal("New Name", module.Name);
+        }
+
+        [Fact]
+        public void TestNotifyChange()
+        {
+            Module module = new Module { Name = "Old Name" };
+            ModuleVM vm = new ModuleVM(module);
+            bool eventTriggered = false;
+
+            vm.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == nameof(vm.Name))
+                {
+                    eventTriggered = true;
+                }
+            };
+
+            vm.Name = "New Name";
+
+            Assert.True(eventTriggered);
+        }
+
+        [Fact]
+        public void TestEmptyConstrutor()
+        {
+            ModuleVM vm = new ModuleVM();
+
+            Assert.NotNull(vm.Model);
+            Assert.IsType<Module>(vm.Model);
+        }
+
+        [Fact]
+        public void TestConstrutorWithModule()
+        {
+            Module module = new Module { Name = "Test Module" };
+
+            ModuleVM vm = new ModuleVM(module);
+
+            Assert.Equal("Test Module", vm.Name);
+            Assert.Equal(module, vm.Model);
+        }
+
+        [Fact]
+        public void Supervisor_SetValue_ShouldUpdateModel()
+        {
+            // Arrange
+            User user = new User { Id = 1, FirstName = "John" };
+            User newUser = new User { Id = 2, FirstName = "Jane" };
+            Module module = new Module { Supervisor = user };
+            ModuleVM vm = new ModuleVM(module);
+
+            // Act
+            vm.Supervisor = new UserVM(newUser);
+
+            // Assert
+            Assert.Equal("Jane", vm.Supervisor.ToString());
+            Assert.Equal(newUser, module.Supervisor);
+        }
+
+
+
+    }
+}
