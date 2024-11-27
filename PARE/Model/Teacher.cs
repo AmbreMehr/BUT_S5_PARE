b1f1c7ff40aec92ff44c2a7832c00b29a2ff0b1c
ClotildeMALO <clotilde.malo@iut-dijon.u-bourgogne.fr>
2024-11-27 16:18:43 +0100

    fix : récupération des heures CM fonctionnelle

--- PARE/Model/Teacher.cs
+++ PARE/Model/Teacher.cs
@@ -89,7 +89,7 @@ namespace Model
                 }
                 else
                 {
-                    assignedTdHours = value;
+                    assignedCmHours = value;
                 }
             }
         }
